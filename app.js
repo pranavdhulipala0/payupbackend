7e94814320d61475183a6987f76c235cc71dcd43
Pranav <pranavdhulipala14@gmail.com>
2023-08-03 02:28:09 +0530

    second commit

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-// const cors = require('cors');
-// app.use(cors({
-//     origin:"myfuturelink"
-// }));
+const cors = require("cors");
+app.use(cors());
 
 async function run() {
   const x = await mongoose.connect(
@@ -18,13 +16,14 @@ run();
 let authSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  rooms: []
+  rooms: [],
 });
 
 let authMod = new mongoose.model("auths", authSchema);
 
 let roomSchema = new mongoose.Schema({
   roomId: { type: Number, required: true },
+  roomName: { type: String, required: true },
   users: [],
   usersData: { type: String, required: true },
   usercount: { type: Number, required: true },
@@ -38,11 +37,12 @@ app.post("/register", (req, res) => {
   var registration = new authMod({
     username: req.body.username,
     password: req.body.password,
-    rooms: []
+    rooms: [],
   });
 
   registration.save();
-  });
+  res.send("User registered!");
+});
 
 function initializer(usersArray, usercount, i) {
   var jsonData = {};
@@ -54,8 +54,26 @@ function initializer(usersArray, usercount, i) {
   return jsonData;
 }
 
+function updater(paymentData, users) {
+  users.forEach((username) => {
+    Object.keys(paymentData.toPay[username]).forEach((key) => {
+      if (
+        paymentData.toPay[username][key] >= paymentData.toBePaid[username][key]
+      ) {
+        paymentData.toPay[username][key] -= paymentData.toBePaid[username][key];
+        paymentData.toBePaid[username][key] = 0;
+        paymentData.toPay[key][username] = 0;
+        paymentData.toBePaid[key][username] = paymentData.toPay[username][key];
+      }
+    });
+  });
+
+  return paymentData;
+}
+
 app.post("/create", async (req, res) => {
   var roomId = req.body.roomId;
+  var roomName = req.body.roomName;
   var usersArray = req.body.users;
   var usercount = usersArray.length;
   //Create the structured data
@@ -72,33 +90,35 @@ app.post("/create", async (req, res) => {
   console.log(finalData);
   var creation = new roomMod({
     roomId: roomId,
+    roomName: roomName,
     users: usersArray,
     usersData: finalData,
     usercount: usercount,
   });
 
   creation.save();
-  for(var i = 0;i<usercount;i++){
-    const temp = await authMod.updateOne({username: usersArray[i]},{ $push: { rooms: roomId }})
+  for (var i = 0; i < usercount; i++) {
+    const temp = await authMod.updateOne(
+      { username: usersArray[i] },
+      {
+        $push: {
+          rooms: { roomId: roomId, roomName: roomName, roomUsers: usersArray },
+        },
+      }
+    );
   }
 
   // Update the user rooms
-
-
-
   res.send("Success");
 });
 
-app.get("/temproute",async (req,res)=>{
-    const temp = await authMod.findOne({username:"drakeswd"})
-    .then((data)=>{
-        console.log(data);
-        data=data.json;
-    }
-    );
+app.get("/temproute", async (req, res) => {
+  const temp = await authMod.findOne({ username: "drakeswd" }).then((data) => {
+    console.log(data);
+    data = data.json;
+  });
 
-    console.log(temp)
-
+  console.log(temp);
 });
 
 app.post("/addUser", async (req, res) => {
@@ -130,8 +150,6 @@ app.post("/addUser", async (req, res) => {
     } else {
       paymentData.toPay[key][newuser] = 0;
     }
-
-
   });
 
   Object.keys(paymentData.toBePaid).forEach(function (key) {
@@ -142,8 +160,11 @@ app.post("/addUser", async (req, res) => {
     }
   });
 
-    //Add room into user directory
-    const temp = await authMod.updateOne({username: new user},{ $push: { rooms: roomId }})
+  //Add room into user directory
+  const temp = await authMod.updateOne(
+    { username: new user() },
+    { $push: { rooms: roomId } }
+  );
 
   mongoData["usersData"] = JSON.stringify(paymentData);
   let updating = await roomMod.findOneAndUpdate({ roomId: roomId }, mongoData);
@@ -168,8 +189,7 @@ app.post("/split", async (req, res) => {
   console.log(splitAmount);
   //ToBePaid
   Object.keys(paymentData.toBePaid[username]).forEach(function (key) {
-    if (paymentData.toPay[key][username] == 0)
-      paymentData.toBePaid[username][key] += splitAmount;
+    paymentData.toBePaid[username][key] += splitAmount;
   });
   //ToPay
   Object.keys(paymentData.toPay).forEach(function (key) {
@@ -179,11 +199,13 @@ app.post("/split", async (req, res) => {
       paymentData.toPay[key][username] += splitAmount;
     }
   });
-
-  console.log("After splitting and updating");
-//   console.log(paymentData);
+  // console.log("After splitting and before updating");
+  // console.log(paymentData);
+  // console.log("After splitting and updating");
+  paymentData = updater(paymentData, mongoData.users);
+  console.log(paymentData);
   mongoData["usersData"] = JSON.stringify(paymentData);
-//   console.log(mongoData);
+  // console.log(mongoData);
 
   let updating = await roomMod.findOneAndUpdate({ roomId: roomId }, mongoData);
   res.send("Success");
@@ -213,8 +235,17 @@ app.post("/login", async (req, res) => {
   });
 });
 
+app.post("/fetchRooms", async (req, res) => {
+  const username = req.body.username;
+  var myRooms = [];
+  const data = await authMod.findOne({ username: username }).then((temp) => {
+    myRooms = temp.rooms;
+  });
+
+  console.log("Request made for: " + username);
+  res.send(JSON.stringify(myRooms));
+});
+
 app.listen(3002, () => {
   console.log("Listening at 3002");
 });
-
-module.exports = app;
